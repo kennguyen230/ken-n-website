ed42f6273305b18549f24468d4ea1c97f42d0def
Kennguyen230 <kennguyen230@gmail.com>
2023-08-14 02:13:04 -0700

    Need to fix gif and info tilt using JS

--- js/project-page.js
+++ js/project-page.js
@@ -1,21 +1,45 @@
 // Project info animation
 function handleSectionAnimation(entries, observer) {
     entries.forEach(entry => {
+        let viewportSize = window.matchMedia("(max-width:700px)")
         const titleElement = entry.target.querySelector(".project-section-title");
         const lineElement = entry.target.querySelector(".line");
         const gifElement = entry.target.querySelector(".animate-gif");
         const infoElement = entry.target.querySelector(".animate-info");
 
-        if (entry.isIntersecting) {
-            titleElement.classList.add("fadeInTop");
-            lineElement.classList.add("animate-line");
-            gifElement.classList.add("fadeInLeft");
-            infoElement.classList.add("fadeInRight");
+        let gifCard = document.querySelector(".GIF-card")
+        let bigGifCard = document.querySelector(".bigger-GIF-card")
+        let goalsAndFeatures = document.querySelector(".features-goals-flex-box")
+
+        // console.log(viewportSize);
+        if (viewportSize.matches) {
+            gifCard.removeAttribute("data-tilt-startX")
+            bigGifCard.removeAttribute("data-tilt-startX")
+            goalsAndFeatures.removeAttribute("data-tilt-startX")
+            
+            if (entry.isIntersecting) {
+                titleElement.classList.add("fadeInTop");
+                lineElement.classList.add("animate-line");
+                gifElement.classList.add("fadeInTop");
+                infoElement.classList.add("fadeInTop");
+            } else {
+                titleElement.classList.remove("fadeInTop");
+                lineElement.classList.remove("animate-line");
+                gifElement.classList.remove("fadeInTop");
+                infoElement.classList.remove("fadeInTop");
+            }
         } else {
-            titleElement.classList.remove("fadeInTop");
-            lineElement.classList.remove("animate-line");
-            gifElement.classList.remove("fadeInLeft");
-            infoElement.classList.remove("fadeInRight");
+            if (entry.isIntersecting) {
+                titleElement.classList.add("fadeInTop");
+                lineElement.classList.add("animate-line");
+                gifElement.classList.add("fadeInLeft");
+                infoElement.classList.add("fadeInRight");
+            } else {
+                titleElement.classList.remove("fadeInTop");
+                lineElement.classList.remove("animate-line");
+                gifElement.classList.remove("fadeInLeft");
+                infoElement.classList.remove("fadeInRight");
+            }
         }
     })
 }
@@ -40,7 +64,7 @@ tunnelmanObserver.observe(document.querySelector("#tunnelman-section"));
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 100) {
+    if (window.scrollY > 100) {
         toTop.classList.add("active");
     } else {
         toTop.classList.remove("active");
